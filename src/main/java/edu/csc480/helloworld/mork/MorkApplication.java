7d9529332c4d52edafe8db74d2c00eadab7e5f4a
Aj <Pyrofreak4321@gmail.com>
2021-02-13 20:38:37 -0500

    add microservice and webpage to ping endpoints

---
+++ src/main/java/edu/csc480/helloworld/mork/MorkApplication.java
@@ -0,0 +1,9 @@
+package edu.csc480.helloworld.mork;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/mork")
+public class MorkApplication extends Application {
+
+}
