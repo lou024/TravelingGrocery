8ad4a5ae913618688fc9f16d20c91550ea0c7be2
Cj Chiasson <45972107+WeStandUnited@users.noreply.github.com>
2021-02-14 20:54:24 -0500

    Monday Thingy

---
+++ src/main/java/edu/csc480/helloworld/cj/CjApp.java
@@ -0,0 +1,12 @@
+package edu.csc480.helloworld.cj;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/Cj")
+public class CjApp extends Application {
+
+
+
+
+}
---
+++ src/main/java/edu/csc480/helloworld/cj/CjServlet.java
@@ -0,0 +1,46 @@
+package edu.csc480.helloworld.cj;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/testmessage")
+public class CjServlet {
+
+  @GET
+   public String getRequest() {
+
+     return "<html>
+
+     <title>TestMessage</title>
+
+     <body>
+
+     <div class="form">
+       <label for="endpoint">Endpoint</label>
+       <input type="text" name="endpoint" spellcheck=false>
+
+       <button type="submit" id="button">Send Ping</button>
+     </div>
+
+
+     <script type="text/javascript">
+       const form = {
+
+           endpoint: document.getElementById('endpoint'),
+           submit: document.getElementById('button')
+
+
+       };
+
+       form.submit.addEventListener('click',()=> {
+         console.log('button pressed!');
+
+
+       });
+
+
+     </script>
+     </body>
+     </html>";
+   }
+}
