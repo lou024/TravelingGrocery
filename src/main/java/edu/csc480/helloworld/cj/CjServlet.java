f20c4341c6f3d67ab83ba8ecc21a8deb73b86831
Cj Chiasson <45972107+WeStandUnited@users.noreply.github.com>
2021-02-14 23:45:07 -0500

    Fix

--- src/main/java/edu/csc480/helloworld/cj/CjServlet.java
+++ src/main/java/edu/csc480/helloworld/cj/CjServlet.java
@@ -9,38 +9,7 @@ public class CjServlet {
   @GET
    public String getRequest() {
 
-     return "<html>
-
-     <title>TestMessage</title>
-
-     <body>
-
-     <div class="form">
-       <label for="endpoint">Endpoint</label>
-       <input type="text" name="endpoint" spellcheck=false>
-
-       <button type="submit" id="button">Send Ping</button>
-     </div>
-
-
-     <script type="text/javascript">
-       const form = {
-
-           endpoint: document.getElementById('endpoint'),
-           submit: document.getElementById('button')
-
-
-       };
-
-       form.submit.addEventListener('click',()=> {
-         console.log('button pressed!');
-
-
-       });
-
-
-     </script>
-     </body>
-     </html>";
+     return "<b>Hello There</b>";
    }
 }
+
