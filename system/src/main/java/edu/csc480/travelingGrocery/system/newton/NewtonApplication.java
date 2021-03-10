2233088cd19e882e55c996c4dd906ffe8c0cefd0
lou024 <46251647+lou024@users.noreply.github.com>
2021-03-10 17:15:58 -0500

    Delete system directory

--- system/src/main/java/edu/csc480/travelingGrocery/system/newton/NewtonApplication.java
+++
@@ -1,9 +0,0 @@
-package edu.csc480.travelingGrocery.system.newton;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/newton")
-public class NewtonApplication extends Application {
-
-}
--- system/src/main/java/edu/csc480/travelingGrocery/system/treu/TreuApplication.java
+++
@@ -1,9 +0,0 @@
-package edu.csc480.travelingGrocery.system.treu;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/treu")
-public class TreuApplication extends Application {
-
-}
