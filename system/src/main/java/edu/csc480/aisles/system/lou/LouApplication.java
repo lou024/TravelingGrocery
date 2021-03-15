4f8056f5d328da67262c82eef2b62258450d0dcd
lou024 <46251647+lou024@users.noreply.github.com>
2021-03-14 20:03:19 -0400

    Delete system directory

--- system/src/main/java/edu/csc480/aisles/system/lou/LouApplication.java
+++
@@ -1,9 +0,0 @@
-package edu.csc480.aisles.system.lou;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/lou")
-public class LouApplication extends Application {
-
-}
--- system/src/main/java/edu/csc480/aisles/system/mork/MorkApplication.java
+++
@@ -1,9 +0,0 @@
-package edu.csc480.aisles.system.mork;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/mork")
-public class MorkApplication extends Application {
-
-}
